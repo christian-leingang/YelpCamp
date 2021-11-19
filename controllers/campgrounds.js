71aaeefd88daf4f698cabe226264a7725dfcc485
Christian Leingang <chrilei2000@gmail.com>
2021-11-19 18:08:34 +0100

    continued chapter54

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,6 +11,7 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res, next) => {
   const campground = new Campground(req.body.campground);
+  campground.images = req.files.map((file) => ({ url: file.path, filename: file.filename }));
   campground.author = req.user._id;
   await campground.save();
   req.flash('success', 'Campground successfully created!');
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,11 +4,14 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router
   .route('/')
   .get(catchAsync(campgrounds.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+  .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
